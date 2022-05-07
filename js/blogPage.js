c66cc4ce818de966970f44dd664b2a0e797db7f7
Karla <73695367+Noizenne@users.noreply.github.com>
2022-05-07 19:52:26 +0200

    Blog Posts Done

--- js/blogPage.js
+++ js/blogPage.js
@@ -1,11 +1,13 @@
-const blogContainer = document.querySelector(".blogContainer");
+const blogPageContainer = document.querySelector(".blogPageContainer");
 const queryString = document.location.search;
-const params = new URLSearchParams;
-const id = params.get("id");
+const params = new URLSearchParams(queryString);
+const blogId = params.get("id");
 
-console.log(id);
+console.log(blogId);
 
-const url = "https://www.kmrabe.site/wp-json/wp/v2/" + id;
+const url = "https://www.kmrabe.site/wp-json/wp/v2/posts/" + blogId + "?_embed";
+
+
 
 async function getPost() {
     try {
@@ -14,9 +16,46 @@ async function getPost() {
         const data = await response.json();
 
         console.log(data);
+
+        createBlogPost(data);
+
     }
 
     catch {
         console.log("An error has occured.");
     }
 }
+
+getPost();
+
+function createBlogPost(data) {
+
+    blogPageContainer.innerHTML = `
+        <div class = postContainer>
+        <h1>${data.title.rendered}</h1>
+        <img src="${data._embedded["wp:featuredmedia"]["0"].source_url}"</img>
+        <p>${data.content.rendered}</p>
+        </div>
+        `
+}
+
+const blogDate = document.querySelector(".blogDate");
+
+async function getBlogDate() {
+    try {
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        blogDate.innerHTML = `
+        <p>${data.date}</p>`
+
+
+    }
+
+    catch {
+        console.log("An error has occured.");
+    }
+}
+
+getBlogDate();
