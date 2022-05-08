3bff00df6b877b82f45c0667797f5eb07842ee80
Karla <73695367+Noizenne@users.noreply.github.com>
2022-05-08 18:30:00 +0200

    Modal done

--- js/blogPage.js
+++ js/blogPage.js
@@ -3,6 +3,8 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const blogId = params.get("id");
 
+const modalContainer = document.querySelector(".modalContainer");
+
 console.log(blogId);
 
 const url = "https://www.kmrabe.site/wp-json/wp/v2/posts/" + blogId + "?_embed";
@@ -35,8 +37,25 @@ function createBlogPost(data) {
         <h1>${data.title.rendered}</h1>
         <img src="${data._embedded["wp:featuredmedia"]["0"].source_url}"</img>
         <p>${data.content.rendered}</p>
+        <div class = imgContainer>
+        <span>&times;</span>
+        <div class="image">
+        <img src="${data._embedded["wp:featuredmedia"]["0"].source_url}"</img>
         </div>
-        `
+        </div>
+        </div>
+        `  
+       // Modal 
+        document.querySelectorAll(".postContainer img").forEach(image => {
+            image.onclick = () => {
+                document.querySelector(".blogPageContainer .imgContainer").style.display = "block";
+                document.querySelector(".blogPageContainer .imgContainer img").src = image.getAttribute("src");
+            }
+        })
+
+        document.querySelector(".blogPageContainer .imgContainer span").onclick = () => {
+            document.querySelector(".blogPageContainer .imgContainer").style.display = 'none';
+        }
 }
 
 const blogDate = document.querySelector(".blogDate");
@@ -59,4 +78,3 @@ async function getBlogDate() {
 }
 
 getBlogDate();
-
